978a4b4e5e28740518386527daf71d92289b617a
Lagzen Thakuri <thakurizen2@gmail.com>
2024-03-28 20:42:07 +0545

    "Add multiple ring images and map over array of ring objects to display them in the Rings component"
    This commit adds several new ring images and an array of ring objects, each with a name, image, and price. The array is then mapped over to display each ring in the Rings component. The commit also updates the Rings component to use the mapped data instead of a single ring image.

--- src/components/category/ring.jsx
+++ src/components/category/ring.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 import Shopby from "./shop/shopby";
 import Ring1 from "../../assets/rings/ring1.jpg"
+import Ring2 from "../../assets/rings/ring2.jpg"
+import Ring3 from "../../assets/rings/ring3.jpg"
+import Ring4 from "../../assets/rings/ring4.jpg"
+import Ring5 from "../../assets/rings/ring5.jpg"
+import Ring6 from "../../assets/rings/ring6.jpg"
+import Ring7 from "../../assets/rings/ring7.jpg"
 export default function Rings() {
+  let rings = [
+    {
+      "name": "gold ring",
+      "image" : Ring1,
+      "price" : 2000
+    },
+    {
+      "name": "gold ring",
+      "image" : Ring2,
+      "price" : 2000
+    },
+    {
+      "name": "gold ring",
+      "image" : Ring3,
+      "price" : 2000
+    },
+    {
+      "name": "gold ring",
+      "image" : Ring4,
+      "price" : 2000
+    },
+    {
+      "name": "gold ring",
+      "image" : Ring5,
+      "price" : 2000
+    },
+    {
+      "name": "gold ring",
+      "image" : Ring6,
+      "price" : 2000
+    },
+    {
+      "name": "gold ring",
+      "image" : Ring7,
+      "price" : 2000
+    },
+  ]
   return (
     <>
       <div className="h-[1080px] flex m-2">
@@ -9,8 +52,19 @@ export default function Rings() {
 
         <div>
           <h2>Rings</h2>
-          <div>
-            <img src={Ring1} alt="" />
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+            {
+              rings.map((ring)=>{
+                let { name, image , price} = ring;
+                return <>
+                <div className="m-2">
+                  <img src={image} alt="" />
+                  <h2>{name}</h2>
+                  <p>{price}</p>
+                </div>
+                </>
+              })
+            }
           </div>
         </div>
       </div>
